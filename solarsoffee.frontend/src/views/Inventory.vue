d0b80d47d28589f3825b6a531ece63e7304842c4
VasilyGorbunov <vasily.gorbunov@gmail.com>
2020-07-07 15:21:07 +0300

    23

--- solarsoffee.frontend/src/views/Inventory.vue
+++ solarsoffee.frontend/src/views/Inventory.vue
@@ -22,14 +22,19 @@
             </tr>
             <tr v-for="item in inventory" :key="item.id">
                 <td>{{ item.product.name }}</td>
-                <td>{{ item.quantityOnHand }}</td>
+                <td :class="`${applyColor(item.quantityOnHand, item.idealQuantity)}`">
+                    {{ item.quantityOnHand }}
+                </td>
                 <td>{{ item.product.price | price}}</td>
                 <td>
                     <span v-if="item.product.isTaxable">Yes</span>
                     <span v-else>No</span>
                 </td>
                 <td>
-                    <div>X</div>
+                    <div
+                      class="lni lni-cross-circle product-archive"
+                      @click="archiveProduct(item.product.id)"
+                    ></div>
                 </td>
             </tr>
         </table>
@@ -55,8 +60,10 @@
     import NewProductModal from "@/components/modals/NewProductModal.vue";
     import {IShipment} from "@/types/Shipment";
     import {InventoryService} from "@/services/inventory-service";
+    import {ProductService} from "@/services/product-service";
 
     const inventoryService = new InventoryService();
+    const productService = new ProductService();
 
     @Component({
         name: "Inventory",
@@ -67,6 +74,29 @@
         isShipmentVisible: boolean = false;
 
         inventory: IProductInventory[] = [];
+
+        async archiveProduct(productId: number) {
+            await productService.archive(productId);
+            await this.initialize();
+        }
+
+        async saveNewProduct(newProduct: IProduct) {
+            await productService.save(newProduct);
+            this.isNewProductVisible = false;
+            await this.initialize();
+        }
+
+        applyColor(current: number, target: number) {
+            if(current <= 0) {
+                return "red";
+            }
+
+            if (Math.abs(target - current) > 8) {
+                return "yellow";
+            }
+
+            return "green";
+        }
 
         closeModal() {
             this.isShipmentVisible = false;
@@ -102,5 +132,32 @@
 </script>
 
 <style scoped lang="scss">
+    @import "../scss/global";
 
+    .green {
+        font-weight: bold;
+        color: $solar-green;
+    }
+
+    .yellow {
+        font-weight: bold;
+        color: $solar-yellow;
+    }
+
+    .red {
+        font-weight: bold;
+        color: $solar-red;
+    }
+
+    .inventory-actions {
+        display: flex;
+        margin-bottom: 0.8rem;
+    }
+
+    .product-archive {
+        cursor: pointer;
+        font-weight: bold;
+        font-size: 1.2rem;
+        color: $solar-red;
+    }
 </style>
